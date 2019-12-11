44300b91267e9d97b2b6e48f6931f5f730723dc4
yuanm <1093040152@qq.com>
2019-12-11 20:46:44 +0800

    天气

--- 学习爬虫/test.08.py
+++ 学习爬虫/test.08.py
@@ -1,16 +1,74 @@
 #coding=utf-8
+
+import requests,sys,smtplib
 from bs4 import BeautifulSoup
-import requests,os
-# pip install lxml
-all_url = 'https://www.qiushibaike.com/pic/'
-headers = { 'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}  
+from email.mime.text import MIMEText
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
 
-start_html = requests.get(all_url,headers=headers) 
+city = 'shenzhen'
+url = 'https://www.tianqi.com/%s/' % city
 
-soup = BeautifulSoup(start_html.text,'lxml')
-test1 = soup.find('div',class_="col1").find('img')['src']
+headers = { 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
+start_html = requests.get(url,headers=headers)
+demo = start_html.text
+# print (demo)
+soup = BeautifulSoup(demo,'lxml')
 
 
-path = path.strip()
-isExists = os.path.exists(os.path.join("E:/TestSoftware", path))
 
+
+for i in range(1,2):
+    # 查询日期和星期
+    test01 = (soup.find('div', class_="day7").find('ul', class_="week").find_all('li'))[i]
+    test011 = test01.get_text()
+    print u'日期:  %s' % test011
+    # f.write(u'日期:  %s' % test011 + '\n')
+
+    # 查询天气  soup.select_one('ul.txt.txt2')  查询有多个class值的标签
+    test02 = (soup.find('div', class_="day7").select_one('ul.txt.txt2').find_all('li'))[i]
+    test022 = test02.get_text()
+    print u'天气：  %s' % test022
+
+    # f.write(u'天气：  %s' % test022 + '\n')
+
+    # 查询温度
+    test03 = (soup.find('div', class_="day7").find('div', class_="zxt_shuju").find_all('li'))[i]
+    test04 = test03.get_text()
+    # 最高
+    test05 = test04[:2]
+    print u'最高温度:  %s℃' % test05
+    # f.write(u'最高温度:  %s℃' % test05 + '\n')
+
+    # 最低
+    test06 = test04[2:]
+    print u'最低温度:  %s℃' % test06
+    # f.write(u'最低温度:  %s℃' % test06 + '\n')
+
+    # 风向
+    test07 = (soup.find_all('ul', class_="txt"))[1]
+    test08 = (test07.find_all('li'))[i].get_text()
+    print u'风向:  %s' % test08
+    # f.write(u'风向:  %s' % test08 + '\n')
+    # f.write('----------------------------------------' + '\n')
+    if test022==u'晴':
+        print u'明天天气:%s，请注意防晒哟~'%test022
+    elif test022==u'雨':
+        print u'明天天气:%s，记得带伞哈~'%test022
+    else:
+        print u'明天天气:%s，注意天气变化~'%test022
+
+    if int(test05)-int(test06)>=10:
+        print u'明天温差较大，冷热交替，小心感冒流鼻涕，哈哈~'
+    else:
+        print u'明天温差不大，适合晒太阳，记得出来走走，赶走霉运~'
+
+    if int(test06)>=18:
+        print u'明天夜晚最低温度:%s℃，可根据实际情况决定被子的厚度~' %test06
+    else:
+        print u'明天夜晚最低温度:%s℃，请盖好被子，盖好小jiojio，可别感冒了哟~' %test06
+
+
+
+
+#12月12日星期四 晴 24 14 东风
--- 学习爬虫/天气.py
+++ 学习爬虫/天气.py
@@ -21,10 +21,68 @@ def tianqi():
     delete()
 
     with open('E:\\test\\weather.txt', 'a') as f:
-        f.write(u'亲，以下是未来一周的天气，查收哦 ~' + '\n\n')
+
 
         soup = city_url(url)
 
+        for i in range(1, 2):
+            # 查询日期和星期
+            # test01 = (soup.find('div', class_="day7").find('ul', class_="week").find_all('li'))[i]
+            # test011 = test01.get_text()
+            # print u'日期:  %s' % test011
+            # f.write(u'日期:  %s' % test011 + '\n')
+
+            # 查询天气  soup.select_one('ul.txt.txt2')  查询有多个class值的标签
+            test02 = (soup.find('div', class_="day7").select_one('ul.txt.txt2').find_all('li'))[i]
+            test022 = test02.get_text()
+            # print u'天气：  %s' % test022
+
+            # f.write(u'天气：  %s' % test022 + '\n')
+
+            # 查询温度
+            test03 = (soup.find('div', class_="day7").find('div', class_="zxt_shuju").find_all('li'))[i]
+            test04 = test03.get_text()
+            # 最高
+            test05 = test04[:2]
+            # print u'最高温度:  %s℃' % test05
+            # f.write(u'最高温度:  %s℃' % test05 + '\n')
+
+            # 最低
+            test06 = test04[2:]
+            # print u'最低温度:  %s℃' % test06
+            # f.write(u'最低温度:  %s℃' % test06 + '\n')
+
+            # 风向
+            test07 = (soup.find_all('ul', class_="txt"))[1]
+            test08 = (test07.find_all('li'))[i].get_text()
+            # print u'风向:  %s' % test08
+            # f.write(u'风向:  %s' % test08 + '\n')
+            # f.write('----------------------------------------' + '\n')
+            if test022 == u'晴':
+                print u'明天天气:%s，请注意防晒哟~' % test022
+                f.write('\n'+u'明天天气:%s，目之所及，皆是回忆~' % test022 + '\n')
+            elif test022 == u'雨':
+                print u'明天天气:%s，记得带伞哈~' % test022
+                f.write('\n'+u'明天天气:%s，记得带伞哈~' % test022 + '\n')
+            else:
+                print u'明天天气:%s，这个天气变化多端，注意天气变化~' % test022
+                f.write('\n'+u'明天天气:%s，这个天气变化多端，听天由命咯~' % test022 + '\n')
+
+            if int(test05) - int(test06) >= 10:
+                print u'明天温差较大，冷热交替，小心感冒流鼻涕，哈哈~'
+                f.write(u'明天温差较大，冷热交替，小心感冒流鼻涕，哈哈~'+'\n')
+            else:
+                print u'明天温差不大，适合晒太阳，记得出来走走，赶走霉运~'
+                f.write(u'明天温差不大，适合晒太阳，记得出来走走，赶走霉运~' + '\n')
+
+            if int(test06) >= 18:
+                print u'明天夜晚最低温度:%s℃，可根据实际情况决定被子的厚度~' % test06
+                f.write(u'明天夜晚最低温度:%s℃，可根据实际情况决定被子的厚度~' % test06 + '\n\n\n')
+            else:
+                print u'明天夜晚最低温度:%s℃，请盖好被子，盖好小jiojio，可别感冒了哟~' % test06
+                f.write(u'明天夜晚最低温度:%s℃，请盖好被子，盖好小jiojio，我要开始讲故事了~' % test06 + '\n\n\n')
+
+        f.write(u'亲，以下是未来一周的天气，请查收 ~' + '\n\n')
         for i in range(7):
             # 查询日期和星期
             test01 = (soup.find('div',class_="day7").find('ul',class_="week").find_all('li'))[i]
@@ -38,6 +96,7 @@ def tianqi():
             test022 = test02.get_text()
             print u'天气：  %s'%test022
             f.write(u'天气：  %s'%test022+'\n')
+
 
 
             #查询温度
@@ -89,13 +148,14 @@ def Email():
     mail_host="smtp.qq.com" #设置服务器
     mail_user="1093040152@qq.com" #用户名
     mail_pass="kzqownmlsecdgedd" # QQ邮箱登录的授权码
-    receivers =['袁猛<yuanm@leyoujia.com>','浮夸<1719422952@qq.com>','猛子<1093040152@qq.com>']
+    receivers =['袁猛<1093040152@qq.com>','丽丽<1032158846@qq.com>','维维<283562659@qq.com>']
     # 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
     message = MIMEText(content, 'plain', 'utf-8') #文本内容
-    message['From'] = mail_user # 发送者   （发送人同QQ备注，可不写）
+    message['From'] ='袁猛<1093040152@qq.com>' #mail_user # 发送者   （发送人同QQ备注，可不写）
     message['To'] = ','.join(receivers) # 这里必须要把多个邮箱按照逗号拼接为字符串
-    subject = u'你在看天气，而我在看你'  #主题
+    subject = u'妳在看天气，而我在看妳'  #主题
     message['Subject'] = subject
+
     try:
         c = smtplib.SMTP()
         c.connect(mail_host, 25) # 25 为 SMTP 端口号
