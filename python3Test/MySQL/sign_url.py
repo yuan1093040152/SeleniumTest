8dab117065c78cb35dce98bf70cdc30fc198abf9
yuanm <1093040152@qq.com>
2020-12-31 09:48:28 +0800

    123

---
+++ python3Test/MySQL/sign_url.py
@@ -0,0 +1,47 @@
+#coding=utf-8
+import MySQLdb,time,argparse
+
+
+def get_htbh(HTBH):
+
+    sql = "SELECT * FROM HT_MAIN a RIGHT JOIN HT_FDD_SIGNER b  ON a.ID = b.HT_ID WHERE b.SIGN_STATUS = '7' AND a.HTBH = '%s';"%HTBH
+    #sql = "SELECT b.ID,b.HT_ID FROM HT_MAIN a RIGHT JOIN HT_FDD_SIGNER b  ON a.ID = b.HT_ID WHERE a.HTBH = '%s';"%HTBH
+
+    db = MySQLdb.connect(host='172.16.22.101', user='root', passwd='admintest', port=33096, db='jjsht',charset='utf8')  # 打开数据库连接
+    cur = db.cursor()  # 使用cursor()方法获取操作游标
+    cur.execute(sql)  # 使用execute方法执行SQL语句
+    db.commit()  # 提交请求
+    values = cur.fetchall()  # 使用 fetchone() 方法获取一条数据
+    cur.close()  # 关闭数据库连接
+    # return values
+
+    print(values)
+    try:
+
+        HT_ID = values[0][1]
+        print(HT_ID)
+        for i in values:
+            QS_ID = i[0]
+            print(QS_ID)
+
+            a = 'http://172.16.22.100/jjsht/fddThird/gotoSignPage/%s/%s'%(HT_ID,QS_ID)
+            print(a)
+    except:
+        print('未找到需要签署的信息，请确认签署人是否已实名认证')
+        pass
+
+    return
+
+if __name__ == '__main__':
+    time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+    # 获取jenkins传递过来的参数
+    parser = argparse.ArgumentParser()
+    parser.add_argument("emp_number")
+    args = parser.parse_args()
+    param = vars(args)
+    HTBH = param['emp_number']
+
+    get_htbh(HTBH)
+
+
