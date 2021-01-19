3e4fb8c0db558503225b827cef4397d9c18e4533
yuanm <1093040152@qq.com>
2021-01-19 09:31:17 +0800

    123

--- python3Test/接口/ApplyID_Pass.py
+++ python3Test/接口/ApplyID_Pass.py
@@ -12,22 +12,10 @@ Ctrl+/       快速注释
 
 import MySQLdb,time,argparse,requests,json
 
-def apply_id(HTBH,apply,url1,url2):
 
-    ht_sql = "SELECT b.APPLY_ID FROM HT_MAIN a RIGHT JOIN APPLY_COMMON b ON a.ID = b.GL_ID WHERE HTBH = '%s'"%HTBH
-    #sql = "SELECT b.ID,b.HT_ID FROM HT_MAIN a RIGHT JOIN HT_FDD_SIGNER b  ON a.ID = b.HT_ID WHERE a.HTBH = '%s';"%HTBH
-    print(ht_sql)
-    ht = MySQLdb.connect(host='172.16.22.101', user='root', passwd='admintest', port=33096, db='jjsht',charset='utf8')  # 打开数据库连接
-    ht_cur = ht.cursor()  # 使用cursor()方法获取操作游标
-    ht_cur.execute(ht_sql)  # 使用execute方法执行SQL语句
-    ht.commit()  # 提交请求
-    ht_values = ht_cur.fetchall()  # 使用 fetchone() 方法获取一条数据
-    applyId = (ht_values[0][0])
-    print(applyId)
-    ht_cur.close()  # 关闭数据库连接
+def apply_id(ApplyID,url1,url2):
 
-
-    hr_sql = "SELECT * FROM APPROVAL_PEOPLE WHERE APPLY_ID = '%s' ORDER BY ORDER_NUMBER;"%apply
+    hr_sql = "SELECT * FROM APPROVAL_PEOPLE WHERE APPLY_ID = '%s' AND APPROVAL_STATUS IN (1,2) ORDER BY ORDER_NUMBER;"%ApplyID
     print(hr_sql)
     hr = MySQLdb.connect(host='172.16.22.101', user='root', passwd='admintest', port=33096, db='workflowplatform', charset='utf8')
     hr_cur = hr.cursor()
@@ -55,7 +43,6 @@ def apply_id(HTBH,apply,url1,url2):
         worker_no = hr_no_values[0][0]
         print(worker_no)
 
-        #body = 'msgBody={"workerNo":"000012","macAddress":"6C-0B-84-A4-72-DD","wpassword":"123456","ckNum":"654321","codeVal":"0000","needCkNum":"true","hddid":"","handInfo":""}'
         body = 'workerNo='+str(worker_no)+'&wpassword=123456&ckNum=654321&codeVal=0000&needCkNum=true&hddid=&handInfo=&macAddress=6C-0B-84-A4-72-DD'
         headers = {
             'Host': '172.16.22.100',
@@ -64,18 +51,11 @@ def apply_id(HTBH,apply,url1,url2):
             'X-Requested-With': 'XMLHttpRequest',
             'Referer': 'http://172.16.22.100/jjslogin/tologin',
             'Origin':'http://172.16.22.100',
-            # 'serviceCode': 'login',
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            # 'v': '3',
             'Accept': 'application/json, text/javascript, */*; q=0.01',
-            # 'empNo': worker_no,
-            # 'appName': 'pc-im',
-            # 'iemi': 'pcMac-6C0B84A472DD',
-            # 'Sec-Fetch-Site': 'same-site',
             'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'zh-CN,zh;q=0.9'
-            # 'Cookie': 'JSESSIONID=2F16682C3E34234AA071B02DEB46FB6A'
         }
 
         requests.packages.urllib3.disable_warnings()  # 移除SSL警告
@@ -91,7 +71,6 @@ def apply_id(HTBH,apply,url1,url2):
         # token = load['data']['data']['token']
         # print(token)
 
-        #body = 'expandJson=%7B%7D&applyId=1004458&apId=279757413&annexJson=%5B%5D&type=1&approvalContent=testpass'
         body = 'expandJson=%7B%7D&applyId='+str(APPLY_ID)+'&apId='+str(id)+'&annexJson=%5B%5D&type=1&approvalContent=testpass'
 
         headers = {
@@ -101,15 +80,9 @@ def apply_id(HTBH,apply,url1,url2):
             'X-Requested-With': 'XMLHttpRequest',
             'Referer': 'http://172.16.22.100/workflowplatform/applyInfo/detail?applyId=%s'%APPLY_ID,
             'Origin': 'http://172.16.22.100',
-            # 'serviceCode': 'login',
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
             'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            # 'v': '3',
             'Accept': 'application/json, text/javascript, */*; q=0.01',
-            # 'empNo': worker_no,
-            # 'appName': 'pc-im',
-            # 'iemi': 'pcMac-6C0B84A472DD',
-            # 'Sec-Fetch-Site': 'same-site',
             'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'zh-CN,zh;q=0.9',
             'Cookie':'JSESSIONID=%s;login-workerid=%s'%(JSESSIONID,WORKER_ID)
@@ -119,15 +92,23 @@ def apply_id(HTBH,apply,url1,url2):
         response = requests.request("POST", url2, data=body, headers=headers, verify=False)  # ssl验证:verify = False
         print(response.text)
 
-
-    return applyId
+    return hr_values
 
 
 
 
 if __name__ == '__main__':
-    HTBH = '58000000062'
-    apply = '1004602'
+    # HTBH = 'ht2021011900001'
+    # ApplyID = '1004617'
     url1 = 'http://172.16.22.100/jjslogin/doLoginNew'
     url2 = 'http://172.16.22.100/workflowplatform/applyInfo/approval'
-    apply_id(HTBH,apply,url1,url2)
+    time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+    # 获取jenkins传递过来的参数
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ApplyID")
+    args = parser.parse_args()
+    param = vars(args)
+    ApplyID = param['ApplyID']
+
+    apply_id(ApplyID, url1, url2)
