d1b6cd097ed4127678107331ec1574bc876b06c7
yuanm <yuanm@leyoujia.com>
2021-11-18 19:52:52 +0800

    添加资产UI自动化

--- UI_jjsAutoTest/UI_jjsAsset/page/jggl_page.py
+++ UI_jjsAutoTest/UI_jjsAsset/page/jggl_page.py
@@ -11,7 +11,6 @@ Ctrl+/       快速注释
 """
 
 from UI_jjsAutoTest.UI_jjsAsset.page.login_page import LoginPage
-from UI_jjsAutoTest.UI_jjsAsset.lib import read_config
 import time
 
 #机构管理
@@ -56,17 +55,16 @@ class jgglList(LoginPage):
         "jy_jglx": 'xpath=>//*[@id="contenBody"]/tr[1]/td[10]',
         "jy_jgzt": 'xpath=>//*[@id="contenBody"]/tr[1]/td[13]',
 
-
     }
 
 
+
     def open_url(self):
-        # self.d.open("https://coatest.leyoujia.com/jg/manage/toManageList")
         self.d.open(self.jgurl)
         time.sleep(2)
 
     # 产权类型选择租赁查询
-    def Property_type(self):
+    def Property_type_zl(self):
         self.d.click(self.element_data['cqlx'])
         time.sleep(2)
         self.d.click(self.element_data['cqlx_zl'])
@@ -74,17 +72,154 @@ class jgglList(LoginPage):
         self.d.click( self.element_data['cx'])
         time.sleep(2)
 
+    # 产权类型选择自购物业查询
+    def Property_type_zgwy(self):
+        self.d.click(self.element_data['cqlx'])
+        time.sleep(2)
+        self.d.click(self.element_data['cqlx_zgwy'])
+        time.sleep(2)
+        self.d.click(self.element_data['cx'])
+        time.sleep(2)
+
+
+    #产权类型校验
+    def Property_type_check(self):
+        self.d.get_text(self.element_data["jy_cqlx"])
+        time.sleep(3)
+
+
+    #归属公司选择深圳查询
+    def company_sz(self):
+        self.d.click(self.element_data['gsgs'])
+        time.sleep(2)
+        self.d.click(self.element_data['gsgs_sz'])
+        time.sleep(2)
+        self.d.click(self.element_data['cx'])
+        time.sleep(2)
+
+    #归属公司选择中山查询
+    def company_zs(self):
+        self.d.click(self.element_data['gsgs'])
+        time.sleep(2)
+        self.d.click(self.element_data['gsgs_zs'])
+        time.sleep(2)
+        self.d.click(self.element_data['cx'])
+        time.sleep(2)
+
+    #归属公司校验
+    def company_check(self):
+        self.d.get_text(self.element_data["jy_gsgs"])
+        time.sleep(3)
+
+
+    #省市区查询
+    def areas(self):
+        self.d.click(self.element_data['sf'])
+        time.sleep(2)
+        self.d.click(self.element_data['sf_gd'])
+        time.sleep(2)
+        self.d.click(self.element_data['cs'])
+        time.sleep(2)
+        self.d.click(self.element_data['cs_sz'])
+        time.sleep(2)
+        self.d.click(self.element_data['cq'])
+        time.sleep(2)
+        self.d.click(self.element_data['cq_ns'])
+        time.sleep(2)
+        self.d.click(self.element_data['cx'])
+        time.sleep(2)
+
+    #省市区校验
+    def areas_check(self):
+        self.d.get_text(self.element_data["jy_cq"])
+        time.sleep(3)
+
+
+    #机构类型选择集团查询
+    def organization_type_jt(self):
+        self.d.click(self.element_data['jglx'])
+        time.sleep(2)
+        self.d.click(self.element_data['jglx_jt'])
+        time.sleep(2)
+        self.d.click(self.element_data['cx'])
+
+    # 机构类型选择子公司查询
+    def organization_type_zgs(self):
+        self.d.click(self.element_data['jglx'])
+        time.sleep(2)
+        self.d.click(self.element_data['jglx_zgs'])
+        time.sleep(2)
+        self.d.click(self.element_data['cx'])
+
+    # 机构类型选择分支机构查询
+    def organization_type_fzjg(self):
+        self.d.click(self.element_data['jglx'])
+        time.sleep(2)
+        self.d.click(self.element_data['jglx_fzjg'])
+        time.sleep(2)
+        self.d.click(self.element_data['cx'])
+
+    # 机构类型选择其它查询
+    def organization_type_qt(self):
+        self.d.click(self.element_data['jglx'])
+        time.sleep(2)
+        self.d.click(self.element_data['jglx_qt'])
+        time.sleep(2)
+        self.d.click(self.element_data['cx'])
+
+    # 机构类型校验
+    def organization_type_check(self):
+        self.d.get_text(self.element_data["jy_jglx"])
+        time.sleep(3)
+
+
+    # 机构状态选择装修中查询
+    def mechanism_status_zxz(self):
+        self.d.click(self.element_data['jgzt'])
+        time.sleep(2)
+        self.d.click(self.element_data['jgzt_zxz'])
+        time.sleep(2)
+        self.d.click(self.element_data['cx'])
+
+    # 机构状态选择营业中查询
+    def mechanism_status_yyz(self):
+        self.d.click(self.element_data['jgzt'])
+        time.sleep(2)
+        self.d.click(self.element_data['jgzt_yyz'])
+        time.sleep(2)
+        self.d.click(self.element_data['cx'])
+
+    # 机构状态选择已撤铺查询
+    def mechanism_status_ycp(self):
+        self.d.click(self.element_data['jgzt'])
+        time.sleep(2)
+        self.d.click(self.element_data['jgzt_ycp'])
+        time.sleep(2)
+        self.d.click(self.element_data['cx'])
+
+    # 机构状态校验
+    def mechanism_status_check(self):
+        self.d.get_text(self.element_data["jy_jgzt"])
+        time.sleep(3)
+
+
+    # 关键字查询
+    def keyword(self):
+        self.d.send_key(self.element_data['jgzt'],'测试一分行')
+        time.sleep(2)
+        self.d.click(self.element_data['cx'])
+
+
+    # 关键字查询校验
+    def keyword_check(self):
+        self.d.get_text(self.element_data["jy_jgmc"])
+        time.sleep(3)
+
+
 
     # # 关闭网页
     # def close_page(self):
     #     self.d.close()
-
-
-    #校验
-    def check(self):
-        self.d.get_text(self.element_data["accountList01"])
-        time.sleep(3)
-
 
 
 
--- UI_jjsAutoTest/UI_jjsAsset/test_case/test_jggl_select.py
+++ UI_jjsAutoTest/UI_jjsAsset/test_case/test_jggl_select.py
@@ -32,9 +32,92 @@ class jggl_select(TestCase_):
         self.jggl.quit()
 
 
-    def test_Property_type(self):
+    def test_Property_type_zl(self):
         """产权类型租赁查询"""
         self.jggl.open_url()
-        self.jggl.Property_type()
+        self.jggl.Property_type_zl()
+        assert(self.jggl.Property_type_check(),'租赁')
 
 
+    def test_Property_type_zgwy(self):
+        """产权类型自购物业查询"""
+        self.jggl.open_url()
+        self.jggl.Property_type_zgwy()
+        assert(self.jggl.Property_type_check(),'自购物业')
+
+
+    def test_company_sz(self):
+        """归属公司选择深圳查询"""
+        self.jggl.open_url()
+        self.jggl.company_sz()
+        assert(self.jggl.company_check(),'深圳乐有家')
+
+
+    def test_company_zs(self):
+        """归属公司选择中山查询"""
+        self.jggl.open_url()
+        self.jggl.company_zs()
+        assert(self.jggl.company_check(),'中山乐有家')
+
+
+    def test_areas(self):
+        """省市区查询"""
+        self.jggl.open_url()
+        self.jggl.areas()
+        assert(self.jggl.areas_check(),'南山区')
+
+
+    def test_organization_type_jt(self):
+        """机构类型选择集团查询"""
+        self.jggl.open_url()
+        self.jggl.organization_type_jt()
+        assert(self.jggl.organization_type_check(),'集团')
+
+
+    def test_organization_type_zgs(self):
+        """机构类型选择子公司查询"""
+        self.jggl.open_url()
+        self.jggl.organization_type_zgs()
+        assert(self.jggl.organization_type_check(),'子公司')
+
+
+    def test_organization_type_fzjg(self):
+        """机构类型选择分支机构查询"""
+        self.jggl.open_url()
+        self.jggl.organization_type_fzjg()
+        assert(self.jggl.organization_type_check(),'分支机构')
+
+
+    def test_organization_type_qt(self):
+        """机构类型选择其他查询"""
+        self.jggl.open_url()
+        self.jggl.organization_type_qt()
+        assert(self.jggl.organization_type_check(),'其他')
+
+
+    def test_mechanism_status_zxz(self):
+        """机构状态选择装修中查询"""
+        self.jggl.open_url()
+        self.jggl.mechanism_status_zxz()
+        assert(self.jggl.mechanism_status_check(),'装修中')
+
+
+    def test_mechanism_status_yyz(self):
+        """机构状态选择营业中查询"""
+        self.jggl.open_url()
+        self.jggl.mechanism_status_yyz()
+        assert(self.jggl.mechanism_status_check(),'营业中')
+
+
+    def test_mechanism_status_ycp(self):
+        """机构状态选择已撤铺查询"""
+        self.jggl.open_url()
+        self.jggl.mechanism_status_ycp()
+        assert(self.jggl.mechanism_status_check(),'已撤铺')
+
+
+    def test_keyword(self):
+        """关键字查询"""
+        self.jggl.open_url()
+        self.jggl.keyword()
+        assert(self.jggl.keyword_check(),'测试一分行')
