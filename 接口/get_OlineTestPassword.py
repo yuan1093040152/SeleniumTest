7941c3dbeef75286479d43402d165ef015936e5b
yuanm <1093040152@qq.com>
2022-05-16 11:13:35 +0800

    修改线上测试环境访问IP

--- 接口/get_OlineTestPassword.py
+++ 接口/get_OlineTestPassword.py
@@ -5,7 +5,7 @@ import requests,json,time,smtplib,re
 def get_sessionID(url):
 	body = 'loginName=tandf&loginPass=123456'
 	headers = {
-	'Host':'172.16.4.223:12001',
+	'Host':'172.16.3.233:12001',
 	'Connection': 'keep-alive',
 	'Content-Length': '32',
 	'Accept': 'application/json, text/javascript, */*; q=0.01',
@@ -13,8 +13,8 @@ def get_sessionID(url):
 	'sessionID': 'null',
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
 	'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-	'Origin': 'http://172.16.4.223:12001',
-	'Referer': 'http://172.16.4.223:12001/',
+	'Origin': 'http://172.16.3.233:12001',
+	'Referer': 'http://172.16.3.233:12001/',
 	'Accept-Encoding': 'gzip, deflate',
 	'Accept-Language': 'zh-CN,zh;q=0.9',
 	'Cookie': '_ga=GA1.1.1238046553.1594955626'	
@@ -31,15 +31,15 @@ def get_sessionID(url):
 def get_OnlinetestPassword(url1):
 	sessionID = get_sessionID(url)
 	headers = {
-	'Host': '172.16.4.223:12001',
+	'Host': '172.16.3.233:12001',
 	'Connection': 'keep-alive',
 	'Content-Length': '0',
 	'Accept': 'application/json, text/javascript, */*; q=0.01',
 	'X-Requested-With':'XMLHttpRequest',
 	'sessionID':sessionID ,
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
-	'Origin': 'http://172.16.4.223:12001',
-	'Referer': 'http://172.16.4.223:12001/portal/index',
+	'Origin': 'http://172.16.3.233:12001',
+	'Referer': 'http://172.16.3.233:12001/portal/index',
 	'Accept-Encoding': 'gzip, deflate',
 	'Accept-Language': 'zh-CN,zh;q=0.9',
 	'Cookie': '_ga=GA1.1.1238046553.1594955626'
@@ -190,8 +190,8 @@ def IMsendinfo(ids, text, info,group='im-serve-attend',url=''):
 
 
 if __name__ == '__main__':
-	url1 = 'http://172.16.4.223:12001/apis/back/oldSystem/PassGet'
-	url = 'http://172.16.4.223:12001/privilege/front/users/login'
+	url1 = 'http://172.16.3.233:12001/apis/back/oldSystem/PassGet'
+	url = 'http://172.16.3.233:12001/privilege/front/users/login'
 	rs_time = get_OnlinetestPassword(url1)
 	ids = ["252613","388809","446957","404499"]
 	text = rs_time
