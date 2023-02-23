5ae5b5c02e0d78db14c20022c19279faf54d6849
yuanm <1093040152@qq.com>
2023-02-23 19:13:52 +0800

    哈哈哈2

---
+++ python3Test/Test/test012.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+"""
+@Author  : Yuan Meng
+@Time    : 2023/2/23 19:13
+@Software: PyCharm
+Ctrl+shift+v 历史粘贴版
+ctrl+alt+空格 自动补全
+ctrl+alt+D   分屏
+Ctrl+/       快速注释
+
+"""
+
+print("123")
