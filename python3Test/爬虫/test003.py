cf0a6049abbfb46669ef82db955d9efa5f22e530
yuanm <1093040152@qq.com>
2023-05-11 16:57:51 +0800

    新的提示打卡

---
+++ python3Test/爬虫/test003.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+"""
+@Author  : Yuan Meng
+@Time    : 2023/5/11 11:22
+@Software: PyCharm
+Ctrl+shift+v 历史粘贴版
+ctrl+alt+空格 自动补全
+ctrl+alt+D   分屏
+Ctrl+/       快速注释
+
+"""
+
+import requests,json
+
+url = "pl:NXXR"
+print(url[-4:])
+
