2237c4c89bef38d715ff4e545ed36e972c0c15b8
yuanm <1093040152@qq.com>
2024-04-23 16:37:02 +0800

    鼠标左击

---
+++ python3Test/Test/Left_click_mouse.py
@@ -0,0 +1,26 @@
+#coding=utf-8
+"""
+@Author  : Yuan Meng
+@Time    : 2024/4/23 16:21
+@Software: PyCharm
+Ctrl+shift+v 历史粘贴版
+ctrl+alt+空格 自动补全
+ctrl+alt+D   分屏
+Ctrl+/       快速注释
+
+
+模拟鼠标左击
+"""
+
+import pyautogui,time
+
+time.sleep(5)
+#获取鼠标的位置
+mouse_position = pyautogui.position()
+for i in range(1,51):
+    time.sleep(2)
+    #点击左键
+    pyautogui.mouseDown(mouse_position)
+    #松开左键
+    pyautogui.mouseUp(mouse_position)
+    print(i)
