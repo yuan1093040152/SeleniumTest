f61bcf94e2e57a12f27f7c5150fd785b9a76a1c8
yuanm <1093040152@qq.com>
2024-08-23 17:08:18 +0800

    fix：修复3

--- 接口/get_OlineTestPassword.py
+++ 接口/get_OlineTestPassword.py
@@ -209,9 +209,9 @@ def IMsendinfo(ids, text, info,group='im-serve-attend',url=''):
 """
 1、利用消息平台发送UAT密码给相关人员
 """
-def XXPT_fs(txt,title,emp_number,emp_name):
-    url = "https://i.leyoujia.com/msg/im/addTemplateInfo"
-    data ={
+def XXPT_fs(txt,title,emp_number,emp_name,Cookie):
+	url = "https://i.leyoujia.com/msg/im/addTemplateInfo"
+	data ={
         "belongDept":"12",
         "laterRemind":"1",
         "pushMode": "1",
@@ -227,15 +227,15 @@ def XXPT_fs(txt,title,emp_number,emp_name):
                              "value": emp_number,
                              "name": emp_name}
                             ]
-    }
-    headers = {"Accept":"application/json, text/javascript, */*; q=0.01",
+		}
+	headers = {"Accept":"application/json, text/javascript, */*; q=0.01",
                "Accept-Encoding":"gzip, deflate, br",
                "Accept-Language":"zh-CN,zh;q=0.9",
                "Cache-Control":"no-cache",
                "Connection":"keep-alive",
                "Content-Length":"464",
                "Content-Type":"application/json; charset=UTF-8",
-               "Cookie":"prefs={}; fhListCookies=; jjshome_uuid=2d8db24a-d505-94df-befa-fce33a6057be; _ga=GA1.2.469380103.1688031287; /hsl/index/house-list_guidance=1; cookiesId=67fd28febc684aa2b689cff24c43107f; ysl-list=1; reserve-list=1; token=t.ORD0x1QdtR11YEgLgNn0; gr_user_id=3fab8280-a3be-4c3e-bb5d-e2517dd30cd9; connect.sid=s%3A85-aogZtViP5TpwyLIgr5Dfqhj9R0Zb1.%2FdFAOCuSJ4XDikYu6bzsKwowklPrcx62icgEKERUM3g; JSESSIONID-FANG=MWY3Y2IzMDUtNzI2MS00ZWRkLTliMTYtNmQxNGZhZDI3ZDA0; JSESSIONID=8C62D5150084F58A28CB4B8D6072A1D9; proLEYOUJIA=MDJlYzFhOTItMzIzMi00YWRiLWIwNmMtMDdkMzgzYmE3YWE0; login-mac=; jjshome_sid=c6a53232-74a1-7b26-9781-61bc63dd5f5f; login-workerid=33029115; fatLEYOUJIA=N2U5YTIwNTctOTAxNS00ZTYzLWIwYmItNDlmZDkxZDk5MmE2",
+               "Cookie":Cookie,
                "Host":"i.leyoujia.com",
                "Origin":"https://i.leyoujia.com",
                "Pragma":"no-cache",
@@ -245,9 +245,10 @@ def XXPT_fs(txt,title,emp_number,emp_name):
                "Sec-Fetch-Site":"same-origin",
                "User-Agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36",
                "X-Requested-With":"XMLHttpRequest"}
-    response = requests.post(url, data=json.dumps(data), headers=headers)
-    s1 = response.json()
-    return s1
+	response = requests.post(url, data=json.dumps(data), headers=headers)
+	print('response======',response)
+	s1 = response.json()
+	return s1
 
 
 """
@@ -256,11 +257,11 @@ title:发送的标题
 text:发送的内容
 """
 def js(ids,text,title):
-    for i in ids:
-        emp_number = list(i.keys())[0]
-        emp_name = list(i.values())[0]
-        sendinfo = XXPT_fs(text,title,emp_number,emp_name)
-        print(emp_number,emp_name)
+	for i in ids:
+		emp_number = list(i.keys())[0]
+		emp_name = list(i.values())[0]
+		sendinfo = XXPT_fs(text,title,emp_number,emp_name,Cookie)
+		print(emp_number,emp_name)
 
 
 
@@ -272,11 +273,13 @@ if __name__ == '__main__':
 	# ids = ["袁猛","曾亮","汪永喜","孙杰","苏薇","杨耿晖","李珍一","黄慧","曾佩","王曼莹","刘颖","冉成浩"]
 	#ids = ["252613","454949","405984","268709","104667","407662","428606","045682","029246","190539","265727","410622"]
 	# ids = ["252613"]
-	ids = [{'06045224': '袁猛'}, {'00410622': '冉成浩'}, {'77807633': '黄慧'}, {'02081317': '王曼莹'}, {'06058331': '刘颖'}]
+	# ids = [{'06045224': '袁猛'}, {'00410622': '冉成浩'},  {'02081317': '王曼莹'}, {'06058331': '刘颖'}]
+	ids = [{'06045224': '袁猛'}]
 	print(ids)
 	text = rs_time
 	title = 'UAT验收环境登陆密码'
-	# js(ids,text,title)
+	Cookie = 'jjshome_uuid=891fccac-b5eb-1f92-4e22-5939da7824c3; prefs={}; gr_user_id=06281673-fdf3-4867-903f-bcf9a6ccc70e; cookiesId=e5ee328c2dbf446e8a58f673a6176bc1; agentCardhd_time=1; fhListCookies=; /hsl/index/house-list_guidance=1; Hm_lvt_1851e6f08c8180e1e7b5e33fb40c4b08=1710320509; Hm_lvt_728857c2e6b321292b2eb422213d1609=1710320509; token=t.gIN6vT27o5V0xhHA41iW; JSESSIONID=21D88BD23B4443BA399643AEF0209705; fatLEYOUJIA=YzBjMmIxY2YtZDkzOC00NGRmLTk2NjgtZjkzYzhiOGFlMTUz; login-workerid=06045224; lyj_pc_token=XCE7TuPxFgcWmcocMiGlSTNErDNpB4jt; proLEYOUJIA=M2Q2ZWM4ZDEtZjEzNC00ODhjLTk0MjItZDcwMDUxZWQ1Y2Ri; login-mac=dc02f04c52b58e4751f9223b9d2fe776; jjshome_sid=48d2682f-98fc-6918-c2e8-fe96f6fb5ce2'
+	js(ids,text,title)
 
 	# IMsendinfo(ids, text, info,group='im-serve-attend',url='')
 	# Email(rs_time)
