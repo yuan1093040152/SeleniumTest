0b4aeb6333634fd7931ec5990831f1267f740768
yuanm <1093040152@qq.com>
2025-09-01 16:10:00 +0800

    2222

---
+++ ht-online/ht_saveHtForCommon.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+"""
+@Author  : Yuan Meng
+@File    : ht_saveHtForCommon.py
+@Time    : 2025/9/1 15:56
+@Software: PyCharm
+Ctrl+shift+v 历史粘贴版
+ctrl+alt+空格 自动补全
+ctrl+alt+D   分屏
+Ctrl+/       快速注释
+
+"""
+
+
